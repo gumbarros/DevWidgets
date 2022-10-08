1ebf6ea4e7d06e305d934b10bd7a231002cab74f
gumbarros <gumbarros@users.noreply.github.com>
2022-10-08 19:09:44 +0000

    deploy: 7adf8e017df5100be48284f3904e48909e71831f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "main.dart.js": "df81cf33459dc61f39ac1fb395d434bc",
 "favicon.ico": "5b04a15c57dc0cd20ddcb0a2975333de",
-"index.html": "b23e0885683b51abb23144b370a60f5c",
-"/": "b23e0885683b51abb23144b370a60f5c",
+"index.html": "2cc80ccfde560a23edea8efd15e1a318",
+"/": "2cc80ccfde560a23edea8efd15e1a318",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "manifest.json": "0907b258ad280d8d71bea0162c8fdb5a",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",

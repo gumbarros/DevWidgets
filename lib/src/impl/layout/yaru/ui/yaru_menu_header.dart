889544f924b7c48e88a3ec4ad4c8701f6c2d3b91
Gustavo Mauricio de Barros <gustavomauriciodebarros@gmail.com>
2022-11-13 12:27:34 -0300

    CI & Menu Fix

--- lib/src/impl/layout/yaru/ui/yaru_menu_header.dart
+++ lib/src/impl/layout/yaru/ui/yaru_menu_header.dart
@@ -1,3 +1,4 @@
+import 'package:dev_widgets/src/impl/layout/yaru/providers/selected_group_provider.dart';
 import 'package:dev_widgets/src/impl/layout/yaru/providers/selected_tool_provider.dart';
 import 'package:dev_widgets/src/impl/settings/settings_provider.dart';
 import 'package:dev_widgets/src/impl/tools.dart';
@@ -11,7 +12,6 @@ class YaruMenuHeader extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-
     final homeTool = getToolByName("home");
 
     final headerTools = [homeTool];
@@ -19,7 +19,8 @@ class YaruMenuHeader extends ConsumerWidget {
     headerTools.addAll(ref
         .watch(settingsProvider)
         .favorites
-        .map((e) => allTools.firstWhere((t) => e == t.name)).toList());
+        .map((e) => allTools.firstWhere((t) => e == t.name))
+        .toList());
 
     return Column(
       children: ListTile.divideTiles(
@@ -30,6 +31,9 @@ class YaruMenuHeader extends ConsumerWidget {
               selected: ref.watch(selectedToolProvider).name == tool.name,
               leading: Icon(tool.icon),
               onTap: () {
+                if (tool.name == "home") {
+                  ref.read(selectedGroupProvider.notifier).state = null;
+                }
                 ref.read(selectedToolProvider.notifier).state = tool;
                 context.go(tool.route);
               },
